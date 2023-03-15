1f69951b7e856c808ce545a31e3798de739fcc30
dimas <aryadimas897@student.uns.ac.id>
2023-03-15 13:47:10 +0700

    Exercise1

---
+++ Exercise1.py
@@ -0,0 +1,11 @@
+print ("Nama : Dimas Arya Santosa")
+print ("NIM  : I0322038")
+print ("Asal : Cilacap")
+
+print ("Saya lahir dan dibesarkan di Cilacap.")
+print ("Cilacap merupakan kota yang indah.")
+print ("Memiliki banyak laut yang cukup memanjakan mata ketika dipandang.")
+print ("Cilacap juga memiliki industri yang cukup besar juga, salah satunya yaitu Pertamina.")
+print ("Cilacap juga merupakan sebuah Kabupaten terluas di Jawa Tengah.")
+
+print ("Harapan saya setelah lulus dari Teknik Industri yaitu saya bisa mengaplikasikan yang telah saya pelajari di dunia kerja.")
